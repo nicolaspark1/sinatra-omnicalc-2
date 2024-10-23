951dcce9b0587a068013d2182682ac9e4c432050
nicolaspark1 <141599069+nicolaspark1@users.noreply.github.com>
2024-10-23 00:40:11 +0000

    updates

--- app.rb
+++ app.rb
@@ -5,40 +5,50 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add/new") do
   erb(:add_form)
 end
 
-get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
-
-  @result = @first_num + @second_num
-  erb(:add_result)
-end
-
-get("/subtract") do
-  erb(:sub_form)
-end
-
-get("/wizard_subtract") do
+get("/add/results") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
 
+  @the_result = @first_num + @second_num
+  erb(:add_result)
+end
+
+get("/sub/new") do
+  erb(:sub_form)
+end
+
+get("/sub/results") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @the_result = @first_num - @second_num
   erb(:sub_result)
 end
 
-get("/multiply") do
-  erb(:multiplication_form)
+get("/multiply/new") do
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/multiply/results") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @the_result = @first_num * @second_num
   erb(:mult_result)
 end
 
-get("/divide") do
+get("/divide/new") do
   erb(:div_form)
 end
 
-get("/wizard_divide") do
+get("/divide/results") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @the_result = @first_num / @second_num
   erb(:div_result)
 end
